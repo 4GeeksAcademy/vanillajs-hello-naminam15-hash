472a5dcbfa1127e3fd11442d2a96faf51bb8e976
Naomi García <naominam15@gmail.com>
2025-11-05 23:51:55 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,31 +4,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-
 let who = ['My theacher', 'My grandma', 'The mailman', 'My bird'];
-let action = [' kicked me out', 'peed', 'crushed', 'broke'];
+let action = ['kicked me out', 'peed', 'crushed', 'broke']; 
 let what = ['of class', 'my phone', 'the car'];
 let when = ['before starting the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 
-function generarExcusaAleatoria() {
-  
+function obtenerElementoAleatorio(lista) {
+  let indiceAleatorio = Math.floor(Math.random() * lista.length);
+  return lista[indiceAleatorio];
+}
 
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
+function generarExcusaAleatoria() {
+    let sujeto = obtenerElementoAleatorio(who);
+    let accion = obtenerElementoAleatorio(action);
+    let objeto = obtenerElementoAleatorio(what);
+    let momento = obtenerElementoAleatorio(when);
 
  
-  return who[whoIndex] + action[actionIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+    return sujeto + " " + accion + " " + objeto + " " + momento;
 }
 
 
-
 window.onload = function() {
- 
-  let elementoExcusa = document.getElementById("excuse");
-  
-
-  elementoExcusa.innerHTML = generarExcusaAleatoria();
+    let elementoExcusa = document.getElementById("excuse");
+    elementoExcusa.innerHTML = generarExcusaAleatoria();
 };
